27734e1131dcb4ef5d84b2df2f65d9edba1c7c62
Natty-ph2 <natnaeldestaer2@gmail.com>
2024-12-09 10:50:21 +0300

    port_upt

--- index.js
+++ index.js
@@ -141,6 +141,6 @@ app.post('/api/persons/', (req, res) => {
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on port${port}`);
   });
 
